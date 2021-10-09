ea9e27cf4dbec3ab6b49fec23989bf99f5e39cd3
Jorge Navas <jdevact@gmail.com>
2021-10-09 19:50:49 -0400

    Updated fill_movie_data script to check if file exists for current date

--- backend/fill_movie_data.sh
+++ backend/fill_movie_data.sh
@@ -1,12 +1,25 @@
 #!/bin/sh
 
-d="http://files.tmdb.org/p/exports/movie_ids_10_03_2021.json.gz"
+# Script to fill postgresql database with movie data from themoviedb daily export files.
+# Will check if file for today's date exists. If not, will check for day before.
+
+today="$(date +'%m_%d_%Y')"
+
+if curl --output /dev/null --silent --head --fail "$url"; then
+    echo "Grabbing daily export file for date: $today"
+else
+    echo "Daily export file for $today does not exist."
+    today="$(date --date=' 1 days ago' '+%m_%d_%Y')"
+    echo "Grabbing daily export file for date: $today"
+fi
+
+url="http://files.tmdb.org/p/exports/movie_ids_${today}.json.gz"
 
 pipe=/tmp/moviefifo
 
 echo "mkfifo $pipe"
 
-request="$(curl "$d" | zcat | jq -r '[.[]] | @csv' >$pipe)"
+request="$(curl "$url" | zcat | jq -r '[.[]] | @csv' >$pipe)"
 
 echo "${request}"
 
