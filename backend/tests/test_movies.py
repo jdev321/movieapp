283300988790626d9d6dbcab284ef807c9c29a0d
Jorge Navas <jdevact@gmail.com>
2021-10-10 20:34:11 -0400

    Updated test_movie response

--- backend/tests/test_movies.py
+++ backend/tests/test_movies.py
@@ -16,4 +16,10 @@ class MovieRoutesTest(unittest.TestCase):
         await movie.save()
         with mock.patch("app.api.crud.random.randint", lambda x, y: movie.movie_id):
             response = await test_app_with_db.get("/movie/")
-        assert response.json()["title"] == "The Matrix"
+        assert response == {
+            "title": "The Matrix",
+            "tagline": "Welcome to the Real World.",
+            "description": "Set in the 22nd century, The Matrix tells the story of a computer hacker who joins a group of underground insurgents fighting the vast and powerful computers who now rule the earth.",
+            "release": "1999-03-30",
+            "poster": "/lh4aGpd3U9rm9B8Oqr6CUgQLtZL.jpg"
+        }
